e3b256b785e362ada0c6e77b6a8da776fa1749a9
Thesoreon <pavels.sus@gmail.com>
2017-11-13 21:32:52 +0100

    - Version of Official Web 2.0

---
+++ script.js
@@ -0,0 +1,14 @@
+var nav = document.getElementById("Navigation");
+
+function ShowMenu() {
+	if(nav.style.display == "none")
+		nav.style.display = "flex";
+	else 
+		nav.style.display = "none";
+}
+
+window.onresize = function() {
+
+	if(window.innerWidth > 600)
+    	nav.style.display = "flex";
+};

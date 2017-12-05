121cc0e5d539cd4c38e86cd46f6716e3155a793b
Thesoreon <pavels.sus@gmail.com>
2017-12-05 22:11:54 +0100

    - Added image gallery
    - Added onclick event for images in gallery -> zoom in a bigger resolution of image
    - Few styles added

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ $(".image").click(function() {
 function testCookies() {
 	alert("COOKIES: " + navigator.cookieEnabled);
 }
+
+LoadImages();
+
+function LoadImages() {
+	var temp = document.getElementsByClassName("image");
+	var modal = document.getElementById("Modal");
+	var modalImg = document.getElementById("img01");
+	var closeButton = document.getElementsByClassName("Close")[0];
+
+	for (var i = 0; i < temp.length; i++) {
+		temp[i].addEventListener("click", function() {
+			modal.style.display = "block";
+			modalImg.src = this.src;			
+		});
+	}
+
+	modal.onclick = function() { 
+  		modal.style.display = "none";
+	}
+}
